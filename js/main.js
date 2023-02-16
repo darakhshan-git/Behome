30331db7b0454c5546a15ba7cdb0a21ac98125bc
Darakhshan <100193642+darakhshan-git@users.noreply.github.com>
2023-02-16 13:49:49 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,287 @@
+
+
+// bedding slider
+ $(document).ready(function() { 
+		$("#logo-demo").owlCarousel({
+			loop:true,
+			items : 4,
+			nav:true,
+			margin: 0,
+			    merge:true,
+
+			navText: ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"],
+			itemsDesktop : [1199,4],
+			itemsDesktopSmall : [767,1],
+			
+			responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:4,
+            nav:true,
+            loop:false
+        }
+    },
+			
+		});
+		
+    });
+	
+	
+	
+	
+	
+	
+	
+	//furnishing
+	 $(document).ready(function() {
+		$("#logo-demo1").owlCarousel({
+			loop:true,
+			items : 4,
+			nav:true,
+			margin: 0,  merge:true,
+			navText: ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"],
+			itemsDesktop : [1199,4],
+			itemsDesktopSmall : [979,1],
+			responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:4,
+            nav:true,
+            loop:false
+        }
+    },
+		});
+		
+		
+    });
+	
+	//bath
+	$(document).ready(function() {	 
+		$("#logo-demo3").owlCarousel({
+			loop:true,
+			items : 4,
+			nav:true,
+			margin: 0,  merge:true,
+			navText: ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"],
+			itemsDesktop : [1199,4],
+			itemsDesktopSmall : [979,1],
+			responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:4,
+            nav:true,
+            loop:false
+        }
+    },
+		});		
+    });
+	
+	//mattresses
+	$(document).ready(function() {	 
+		$("#logo-demo4").owlCarousel({
+			loop:true,
+			items : 4,
+			nav:true,
+			margin: 0, 
+			 merge:true,
+			navText: ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"],
+			itemsDesktop : [1199,4],
+			itemsDesktopSmall : [979,1],
+			responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:4,
+            nav:true,
+            loop:false
+        }
+    },
+		});		
+    });
+	
+	
+	//pillow
+	$(document).ready(function() {	 
+		$("#logo-demo5").owlCarousel({
+			loop:true,
+			items : 4,
+			nav:true,
+			margin: 0,  merge:true,
+			navText: ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"],
+			itemsDesktop : [1199,4],
+			itemsDesktopSmall : [979,1],
+			responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:4,
+            nav:true,
+            loop:false
+        }
+    },
+		});		
+    });
+	
+	
+	//dinning
+	$(document).ready(function() {	 
+		$("#logo-demo6").owlCarousel({
+			loop:true,
+			items : 4,
+			nav:true,
+			margin: 0,  merge:true,
+			navText: ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"],
+			itemsDesktop : [1199,4],
+			itemsDesktopSmall : [979,1],
+			responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:4,
+            nav:true,
+            loop:false
+        }
+    },
+		});		
+    });
+	
+	//kitchen
+	$(document).ready(function() {	 
+		$("#logo-demo7").owlCarousel({	
+			loop:true,
+			items : 4,
+			nav:true,
+			margin: 0,  merge:true,
+			navText: ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"],
+			itemsDesktop : [1199,4],
+			itemsDesktopSmall : [979,1],
+			responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:4,
+            nav:true,
+            loop:false
+        }
+    },
+		});		
+    });
+	
+	
+	//kids
+	$(document).ready(function() {	 
+		$("#logo-demo8").owlCarousel({
+			loop:true,
+			items : 4,
+			nav:true,
+			margin: 0,  merge:true,
+			navText: ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"],
+			itemsDesktop : [1199,4],
+			itemsDesktopSmall : [979,1],
+			responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:4,
+            nav:true,
+            loop:false
+        }
+    },
+		});		
+    });
+	
+
+
+
+// achivements slider
+ $(document).ready(function() { 
+        $("#achv-demo").owlCarousel({
+            loop:true,
+            items : 3,
+            nav:true,
+            margin: 40,
+                merge:true,
+            navText: ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"],
+            itemsDesktop : [1199,3],
+            itemsDesktopSmall : [767,1],
+            
+            responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:3,
+            nav:true,
+            loop:false
+        }
+    },
+            
+        });
+        
+    });
+    
